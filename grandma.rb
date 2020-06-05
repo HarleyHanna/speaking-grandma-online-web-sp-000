b662f3fcace2b868ff644b71258eac3d86ca6b49
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-05 18:54:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(sonny)
   if sonny == sony.upcase && sonny == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
     
